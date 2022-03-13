0f2e6f2a6c3dc863152659ffd93c009bdf0cc158
Gina Napier <gina.napier@miraclesystems.net>
2022-03-13 00:35:12 -0500

    Added Problem 3

--- js/app.js
+++ js/app.js
@@ -32,8 +32,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
- //Add user input together
- let product = a * b; //Add numbers
+ //Calculate product
+ let product = a * b; //Multiply numbers
  let str =`The product of ${a} and ${b} is ${product}.`;
  console.log(str);
 
@@ -56,13 +56,23 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
+
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+ //Calculate product
+ let sum = a + b + c; //Add numbers
+ let product = a * b * c; //Multiply numbers
 
+ let str1 =`${a} and ${b} and ${c} sum to ${sum}.`;
+ let str2 =`The product of ${a} and ${b} and ${c} is ${product}.`;
+ console.log(str1);
+  console.log(str2);
+
+ return [sum,product,str1,str2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
