7be312f0f2508de2c49b5a23a210189a22a14d02
Gina Napier <gina.napier@miraclesystems.net>
2022-03-13 03:45:42 -0400

    Added logic for problem 4

--- js/app.js
+++ js/app.js
@@ -96,13 +96,25 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+//Assign variables for use in addition
+let num1=sumArr[0];
+let num2=sumArr[1];
+let num3=sumArr[2];
+
+//Calculate sum of numbers
+let firstNum = sum(num1,num2)[0];
+let totalSum = sum(firstNum,num3);
+let splitSum = parseInt(totalSum[0]);
+
+//Create the required string for sumArr[0]
+let string1=`${num1},${num2},${num3} was passed in as an array of numbers, and ${splitSum} is their sum.`;
+
+return [splitSum, string1];
 
 }
-
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
-
+testSumArray(testArray);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
